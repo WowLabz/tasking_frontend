dac568d9b2974caffcbfca358c192af4b07f0d83
loaki07 <loakesh.i@gmail.com>
2021-10-18 05:02:23 +0530

    add: getBalance function for the connected accounts and updating the form in the header

--- src/Components/AppHeader/ConnectedAccounts.js
+++ src/Components/AppHeader/ConnectedAccounts.js
@@ -1,18 +1,26 @@
-import React, { useEffect, useState } from "react";
-import { NavDropdown } from "react-bootstrap";
+import React, { useEffect, useRef, useState } from "react";
+import { Button, Form, FormControl, NavDropdown } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
-import { getAccountsFromKeyRing } from "../../palletTaskingFunctions";
+import {
+    DEFAULT_ACCOUNT_IDS,
+    getAccountsFromKeyRing,
+} from "../../palletTaskingFunctions";
 import { useSubstrate } from "../../substrate-lib";
 import { cryptoWalletAccountSelect, cryptoWalletDisconnect } from "./actions";
+import { ApiPromise } from "@polkadot/api";
+import { toast } from "react-toastify";
 
 const connectWalletStyle = {
     border: "1px solid #f2f4f6",
     borderRadius: "12px",
-    minWidth: "7vw"
+    minWidth: "7vw",
     // boxShadow: "4px 4px 4px rgb(151 167 195 / 44%), -4px -4px 4px #f2f4f660"
 };
 
 const ConnectedAccounts = () => {
+    const balanceRef = useRef();
+    const accountNameRef = useRef();
+
     const connectedAccounts = useSelector(
         (state) => state.headerReducer.accountsAvailableInWallet
     );
@@ -22,34 +30,102 @@ const ConnectedAccounts = () => {
     const dispatch = useDispatch();
     const [accountName, setAccountName] = useState("Select Account");
 
-    const handleAccountSelect = (e, acc) => {
-        setAccountName(acc.meta.name);
-        dispatch(cryptoWalletAccountSelect(acc));
+    const handleAccountSelect = async (e) => {
+        try {
+            let currAccountName = e.target.value;
+            let currAcc;
+
+            [...connectedAccounts, ...defaultAccounts]?.forEach(
+                (acc, index) => {
+                    if (acc.meta.name === currAccountName) {
+                        currAcc = acc;
+                    }
+                }
+            );
+
+            setAccountName(currAcc.meta.name);
+            dispatch(cryptoWalletAccountSelect(currAcc));
+
+            await getAndSetBalanceField(currAcc);
+        } catch (error) {
+            console.log(error);
+            toast.error(`Error: ${error}`, {
+                position: toast.POSITION.TOP_RIGHT,
+                autoClose: 3000,
+            });
+        }
     };
 
+    const getAndSetBalanceField = async (currAcc) => {
+        try {
+            const api = await ApiPromise.create();
+
+            let {
+                data: { free: previousFree },
+                nonce: previousNonce,
+            } = await api.query.system.account(currAcc.address);
+            balanceRef.current.value = Math.round(previousFree / 1000000000000);
+
+            console.log(
+                `${currAcc.meta.name} has a balance of ${previousFree}, nonce ${previousNonce}`
+            );
+        } catch (error) {
+            console.log(error);
+            toast.error(`Error: ${error}`, {
+                position: toast.POSITION.TOP_RIGHT,
+                autoClose: 3000,
+            });
+        }
+    };
+
+    useEffect(() => {
+        const accName = accountNameRef.current.value;
+        [...connectedAccounts, ...defaultAccounts]?.forEach((acc, index) => {
+            if (acc.meta.name === accName) {
+                dispatch(cryptoWalletAccountSelect(acc));
+                getAndSetBalanceField(acc);
+            }
+        });
+    }, []);
+
     return (
-        <NavDropdown
-            title={accountName}
-            id="collasible-nav-dropdown"
-            style={connectWalletStyle}
-            className="m-1"
-        >
-            {[...connectedAccounts, ...defaultAccounts]?.map((acc, index) => (
-                <NavDropdown.Item
-                    key={index}
-                    onClick={(e) => handleAccountSelect(e, acc)}
+        <>
+            <Form className="d-flex">
+                <FormControl
+                    as="select"
+                    size="md"
+                    aria-label="Default select example"
+                    className="me-2"
+                    onClick={(e) => handleAccountSelect(e)}
+                    onLoadStart={(e) => handleAccountSelect(e)}
+                    ref={accountNameRef}
                 >
-                    {acc.meta.name}
-                </NavDropdown.Item>
-            ))}
-            <NavDropdown.Item
-                onClick={() => {
-                    dispatch(cryptoWalletDisconnect());
-                }}
-            >
-                Switch Wallet
-            </NavDropdown.Item>
-        </NavDropdown>
+                    {[...connectedAccounts, ...defaultAccounts]?.map(
+                        (acc, index) => (
+                            <option key={index}>{acc.meta.name}</option>
+                        )
+                    )}
+                </FormControl>
+                <Form.Control
+                    className="me-2"
+                    type="text"
+                    placeholder="Balance"
+                    name="accountBalance"
+                    size="md"
+                    readOnly
+                    ref={balanceRef}
+                    disabled
+                />
+                <Button
+                    variant="outline-info"
+                    onClick={() => {
+                        dispatch(cryptoWalletDisconnect());
+                    }}
+                >
+                    <span style={{ fontSize: "12px" }}>Switch&nbsp;Wallet</span>
+                </Button>
+            </Form>
+        </>
     );
 };
 
