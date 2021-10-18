d981b9ef0bf60cd4691fcd5b8166751af1a622d2
loaki07 <loakesh.i@gmail.com>
2021-10-18 19:24:31 +0530

    add: state for taskCard and computing the date and the names

--- src/View/Modules/DashBoard/TaskCard.js
+++ src/View/Modules/DashBoard/TaskCard.js
@@ -9,18 +9,37 @@ import { TASK_STATUS } from "../TaskDetails/constants";
 const TaskCard = ({ data, showFormModal }) => {
     const history = useHistory();
     const [attributesForCard, setAttributesForCard] = useState({});
-    const {
-        task_id,
-        client,
-        worker_id,
-        task_deadline,
-        cost,
-        status,
-        task_description,
-    } = data;
+    const [cardDetails, setCardDetails] = useState({
+        task_id: null,
+        client: null,
+        worker_id: null,
+        task_deadline: null,
+        cost: null,
+        status: null,
+        task_description: null,
+        publisher_name: null,
+        worker_name: null,
+        start_date: null,
+        end_date: null,
+    });
 
-    const publisher =
-        client === constants.DEFAULT_ACCOUNT_IDS.ALICE ? "Alice" : "Bob";
+    const connectedAccounts = useSelector(
+        (state) => state.headerReducer.accountsAvailableInWallet
+    );
+    const defaultAccounts = useSelector(
+        (state) => state.headerReducer.defaultAccounts
+    );
+
+    const getAccountName = (address) => {
+        let res;
+        if (address === null) return null;
+        [...connectedAccounts, ...defaultAccounts].forEach((acc, idx) => {
+            if (address === acc.address) {
+                res = acc.meta.name;
+            }
+        });
+        return res;
+    };
 
     const getAttributesForCard = (status) => {
         switch (status) {
@@ -119,6 +138,28 @@ const TaskCard = ({ data, showFormModal }) => {
     };
 
     const init = () => {
+        const {
+            task_id,
+            client,
+            worker_id,
+            task_deadline,
+            cost,
+            status,
+            task_description,
+        } = data;
+        const publisher_name = getAccountName(client);
+        const worker_name = getAccountName(worker_id);
+
+        let today = new Date();
+        let start_date = today.toLocaleDateString();
+        let end_date = today.addDays(parseInt(task_deadline)).toLocaleDateString();
+        setCardDetails({
+            ...data,
+            publisher_name,
+            worker_name,
+            start_date,
+            end_date,
+        });
         setAttributesForCard(getAttributesForCard(status));
     };
 
@@ -139,12 +180,12 @@ const TaskCard = ({ data, showFormModal }) => {
                 <Card.Body
                     onClick={() =>
                         history.push({
-                            pathname: `/taskdetails/${task_id}`,
+                            pathname: `/taskdetails/${cardDetails.task_id}`,
                         })
                     }
                 >
                     <Card.Text className="d-flex justify-content-between align-items-center">
-                        <b>{_.capitalize(task_description)}</b>
+                        <b>{_.capitalize(cardDetails.task_description)}</b>
                         <Badge
                             variant={attributesForCard.badgeColor}
                             className={`px-2 mx-2`}
@@ -160,35 +201,40 @@ const TaskCard = ({ data, showFormModal }) => {
                                 padding: "0.4rem",
                             }}
                         >
-                            {status}
+                            {cardDetails.status}
                         </Badge>
                     </Card.Text>
                     <hr />
                     <Card.Text>
-                        <b>Publisher:</b> {publisher}
+                        <b>Publisher:</b> {cardDetails.publisher_name}
                     </Card.Text>
+                    {cardDetails.worker_id !== null && (
+                        <Card.Text>
+                            <b>Worker:</b> {cardDetails.worker_name}
+                        </Card.Text>
+                    )}
                     <Card.Text>
-                        <b>TaskId:</b> {task_id}
+                        <b>TaskId:</b> {cardDetails.task_id}
                     </Card.Text>
                     <Card.Text>
                         <b>TaskDeadline: </b>
-                        {task_deadline} days
+                        {cardDetails.task_deadline} days
                     </Card.Text>
                     <Card.Text>
-                        <b>TaskCost:</b> {cost}
+                        <b>TaskCost:</b> {cardDetails.cost}
                     </Card.Text>
                     <div className="d-flex justify-content-between align-items-center">
                         <div>
                             <div>
                                 <b>Start Date</b>
                             </div>
-                            <small>{"31/5/2021"}</small>
+                            <small>{cardDetails.start_date}</small>
                         </div>
                         <div>
                             <div>
                                 <b>End Date</b>
                             </div>
-                            <small>{"24/6/2021"}</small>
+                            <small>{cardDetails.end_date}</small>
                         </div>
                     </div>
                 </Card.Body>
@@ -198,6 +244,12 @@ const TaskCard = ({ data, showFormModal }) => {
             </Card>
         </Col>
     );
+};
+
+Date.prototype.addDays = function (days) {
+    var date = new Date(this.valueOf());
+    date.setDate(date.getDate() + days);
+    return date;
 };
 
 export default TaskCard;
